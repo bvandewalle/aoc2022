c25098616458cacecb58d5ac41b626759d25beaa
Bernard Van De Walle <bvandewalle@splunk.com>
2022-12-05 22:11:59 -0700

    Day6

---
+++ 06/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var input []string
+
+	for scanner.Scan() {
+		v := scanner.Text()
+		input = append(input, v)
+	}
+
+	parts(input, false)
+	parts(input, true)
+}
+
+func parts(input []string, part2 bool) {
+	limit := 4
+	if part2 {
+		limit = 14
+	}
+
+Exit:
+	for i, _ := range input[0] {
+		if i > limit-1 {
+
+			for j := 0; j < limit; j++ {
+				for k := 0; k < limit; k++ {
+					if j != k {
+						if input[0][i-j] == input[0][i-k] {
+							continue Exit
+						}
+					}
+				}
+			}
+			fmt.Println(i)
+			return
+		}
+	}
+}
