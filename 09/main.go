f8b342663f10b59b364121800be757f759470082
Bernard Van De Walle <bvandewalle@splunk.com>
2022-12-08 23:46:37 -0700

    Day9

---
+++ 09/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var input []string
+
+	for scanner.Scan() {
+		v := scanner.Text()
+		input = append(input, v)
+	}
+
+	parts(input, 2)
+	parts(input, 10)
+}
+
+func parts(input []string, ropeLength int) {
+
+	rope := []image.Point{}
+	for i := 0; i < ropeLength; i++ {
+		rope = append(rope, image.Point{X: 0, Y: 0})
+	}
+
+	visited := map[image.Point]bool{}
+
+	for _, iv := range input {
+		a := strings.Split(iv, " ")
+		b, _ := strconv.Atoi(a[1])
+
+		moves := map[string]image.Point{"U": image.Point{0, 1}, "D": image.Point{0, -1}, "R": image.Point{1, 0}, "L": image.Point{-1, 0}}
+
+		toadd := moves[a[0]]
+
+		for i := 0; i < b; i++ {
+			rope[0] = rope[0].Add(toadd)
+
+			for j := 1; j < len(rope); j++ {
+				dx := rope[j-1].X - rope[j].X
+				dy := rope[j-1].Y - rope[j].Y
+				if (dx > 1) || (dx < -1) || (dy > 1) || (dy < -1) {
+					rope[j] = rope[j].Add(image.Point{sign(dx), sign(dy)})
+				}
+			}
+
+			visited[rope[ropeLength-1]] = true
+		}
+	}
+
+	fmt.Println(len(visited))
+}
+
+func sign(dx int) int {
+	if dx < 0 {
+		return -1
+	}
+	if dx > 0 {
+		return 1
+	}
+	return 0
+}
