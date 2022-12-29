2a3078427fd57b684ba69e5d1d11ccd153271451
Bernard Van De Walle <bvandewalle@splunk.com>
2022-12-29 00:26:38 -0700

    Day25

---
+++ 25/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var input []string
+
+	for scanner.Scan() {
+		v := scanner.Text()
+		input = append(input, v)
+	}
+
+	parts(input, false)
+	parts(input, true)
+}
+
+type cube struct {
+	x int
+	y int
+	z int
+}
+
+func parts(input []string, part2 bool) {
+
+	snafu := []int{}
+
+	for _, iv := range input {
+		for i := 0; i < len(iv); i++ {
+			if i+1 > len(snafu) {
+				snafu = append(snafu, 0)
+			}
+			c := iv[len(iv)-i-1]
+			if c == '=' {
+				snafu[i] -= 2
+			} else if c == '-' {
+				snafu[i] -= 1
+			} else {
+				x, _ := strconv.Atoi(string(c))
+				snafu[i] += x
+			}
+		}
+	}
+
+	fmt.Println(snafu)
+
+	for i := range snafu {
+		v := snafu[i]
+		carry := 0
+		for v >= 3 {
+			carry += 1
+			v -= 5
+		}
+		for v <= -3 {
+			carry -= 1
+			v += 5
+		}
+
+		snafu[i] = v
+		if carry != 0 {
+			snafu[i+1] += carry
+		}
+	}
+
+	fmt.Println(snafu)
+
+	for i := len(snafu) - 1; i >= 0; i-- {
+		v := snafu[i]
+		if v == -2 {
+			fmt.Print("=")
+		} else if v == -1 {
+			fmt.Print("-")
+		} else {
+			fmt.Print(v)
+		}
+	}
+	fmt.Println()
+}
